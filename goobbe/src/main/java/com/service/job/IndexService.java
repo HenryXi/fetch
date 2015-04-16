44367b213d5614cdb26683020dea12cafc63632e
Henry Xi <xxy668@foxmail.com>
2015-04-16 22:29:21 +0800

    fix index path issuse

--- goobbe/src/main/java/com/service/job/IndexService.java
+++ goobbe/src/main/java/com/service/job/IndexService.java
@@ -12,6 +12,7 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
@@ -27,14 +28,16 @@ import java.util.List;
 
 @Service
 public class IndexService extends GoobbeLogger {
+    @Autowired
     private JdbcTemplate jdbcTemplate;
+//    private JdbcTemplate jdbcTemplate;-->for test!
     private int INDEX_TITLES_EACH_LOOP=10000;
     private Path indexFolderBak;
     private Path indexFolder;
     private IndexService() {
-        jdbcTemplate=JDBCHelper.createMysqlTemplate("po",
-                "jdbc:postgresql://123.57.136.60:5432/goobbe",
-                "yong", "xixiaoyong123", 80, 120);
+//        jdbcTemplate=JDBCHelper.createMysqlTemplate("po",
+//                "jdbc:postgresql://123.57.136.60:5432/goobbe",
+//                "yong", "xixiaoyong123", 80, 120);
         indexFolderBak= Paths.get(System.getProperty("user.home") + FileSystems.getDefault().getSeparator() + "index_bak");
         indexFolder= Paths.get(System.getProperty("user.home") + FileSystems.getDefault().getSeparator()+"index");
     }
@@ -50,7 +53,7 @@ public class IndexService extends GoobbeLogger {
             Analyzer analyzer = new StandardAnalyzer();
             IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
             IndexWriter writer = new IndexWriter(dir, iwc);
-            int totalNum=jdbcTemplate.queryForInt("select count from tcounter where table_name='tb_content';")/INDEX_TITLES_EACH_LOOP+1;
+            int totalNum=jdbcTemplate.queryForInt("select max(id) from tb_content;")/INDEX_TITLES_EACH_LOOP+1;
             for(int i=1;i<totalNum+1;i++){
                 indexDocs(writer, getQuestionsForIndex(i * INDEX_TITLES_EACH_LOOP));
                 info("indexing... total group: ["+(totalNum+1)+"], ["+INDEX_TITLES_EACH_LOOP+"] items per group, current group: ["+i+"]");
@@ -66,7 +69,8 @@ public class IndexService extends GoobbeLogger {
 
     private Path getIndexPath(){
         try {
-            FileUtils.deleteDirectory(indexFolderBak.toFile());
+            info("getting index path...");
+            info("create index_bak path for indexing.");
             Files.createDirectories(indexFolderBak);
             return indexFolderBak;
         } catch (IOException e) {
