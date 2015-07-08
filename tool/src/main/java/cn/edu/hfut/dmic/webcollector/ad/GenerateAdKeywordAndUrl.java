7b10e4bcbca6da91055e20252ac3b10326427ea3
Henry Xi <xxy668@foxmail.com>
2015-07-08 17:29:31 +0800

    save url and keyword in local db(give up earning money by ad)

--- tool/src/main/java/cn/edu/hfut/dmic/webcollector/ad/GenerateAdKeywordAndUrl.java
+++ tool/src/main/java/cn/edu/hfut/dmic/webcollector/ad/GenerateAdKeywordAndUrl.java
@@ -1,7 +1,11 @@
 package cn.edu.hfut.dmic.webcollector.ad;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.net.URISyntaxException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -14,7 +18,9 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.io.SAXReader;
+import org.jsoup.Jsoup;
 import org.springframework.jdbc.core.JdbcTemplate;
+import sun.net.www.protocol.http.HttpURLConnection;
 
 /**
  * Created by henry on 2015/6/26.
@@ -22,12 +28,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class GenerateAdKeywordAndUrl {
     private JdbcTemplate jdbcTemplate;
     public GenerateAdKeywordAndUrl(){
-//        jdbcTemplate=JDBCHelper.createSQLiteTemplate("SQLite","jdbc:sqlite:"+System.getProperty("user.home")+"url.db",
-//                null, null, 5, 30);
-        jdbcTemplate=JDBCHelper.createPostgresqlTemplate("SQLite","jdbc:postgresql://localhost:15432/page",
-                "kontek", "Passw0rd", 5, 30);
-//        jdbcTemplate.execute("DROP TABLE IF EXISTS url;");
-//        jdbcTemplate.execute("create TABLE url(id varchar ,url varchar);");
+        jdbcTemplate=JDBCHelper.createSQLiteTemplate("SQLite","jdbc:sqlite:"+System.getProperty("user.home")+"/url.db",
+                null, null, 5, 30);
+//        jdbcTemplate=JDBCHelper.createPostgresqlTemplate("SQLite","jdbc:postgresql://localhost:15432/page",
+//                "kontek", "Passw0rd", 5, 30);
+        jdbcTemplate.execute("DROP TABLE IF EXISTS url;");
+        jdbcTemplate.execute("create TABLE url(id varchar ,url varchar,title varchar,description varchar);");
 //        try {
 //            jdbcTemplate.getDataSource().getConnection().setAutoCommit(false);
 //        } catch (SQLException e) {
@@ -38,18 +44,31 @@ public class GenerateAdKeywordAndUrl {
         try {
             File inputFile = new File(this.getClass().getResource("/marketplace_feed_v2.xml").toURI());
             SAXReader reader = new SAXReader();
-            Document document = reader.read( inputFile );
+            Document document = reader.read(inputFile);
 
             System.out.println("Root element :"
                     + document.getRootElement().getName());
-            List<Element> categoryElements=document.getRootElement().elements();
-
             List<Node> nodes = document.selectNodes("/Catalog/Category/Site");
-            List<String> sqls=new ArrayList<>();
+            String adId;
+            String adUrl;
+            String adTitle;
+            String adDescription;
+            StringBuilder sb=new StringBuilder("INSERT into url (id,url,title,description)VALUES");
+            List<String> params=new ArrayList<>();
             for(Node node:nodes){
-                sqls.add("INSERT into url (id,url)VALUES ('" + node.selectSingleNode("Id").getText() + "','henry');");
+                adId=node.selectSingleNode("Id").getText();
+                System.out.println("Id -> " + adId);
+                adUrl="http://goobbe."+adId+".hop.clickbank.net";
+                adTitle=node.selectSingleNode("Title").getText();
+                adDescription=node.selectSingleNode("Description").getText();
+                sb.append("('"+adId+"','"+adUrl+"','"+adTitle+"','"+adDescription+"'),");
+//                params.add()
+//                jdbcTemplate.update("INSERT into url (id,url,title,description)VALUES (?,?,?,?)",
+//                        adId,adUrl,adTitle,adDescription);
             }
-            jdbcTemplate.batchUpdate(sqls.toArray(new String[sqls.size()]));
+
+            sb.deleteCharAt(sb.lastIndexOf(","));
+            jdbcTemplate.queryForList("");
 //            Connection conn = null;
 //            PreparedStatement stmt  = null;
 //            try {
