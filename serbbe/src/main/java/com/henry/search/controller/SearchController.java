96d3fb7cfd98b9b237a24094fbf34c89884c6d79
yong <xxy668@foxmail.com>
2015-11-24 17:50:44 +0800

    init result page

--- serbbe/src/main/java/com/henry/search/controller/SearchController.java
+++ serbbe/src/main/java/com/henry/search/controller/SearchController.java
@@ -1,6 +1,7 @@
 package com.henry.search.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class SearchController {
-    @RequestMapping(value = "/")
-    public String index(){
-        return "index";
+    @RequestMapping(value = "/{page}")
+    public String index(@PathVariable(value = "page") String page){
+        return page;
     }
 }
---
+++ serbbe/src/main/webapp/WEB-INF/views/result.jsp
@@ -0,0 +1,23 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<ul id="m-result" class="result">
+    <li>
+        <h3>
+            <a href="#" target="_blank"><em>targetKey</em>_好搜百科</a>
+        </h3>
+
+        <p class="res-desc">
+            <em>test</em>test test test test test test test test test test test test
+        </p>
+
+        <p>
+            <cite>www.dict.cn/test</cite>-<a href="#" target="_blank" class="m">快照</a>
+        </p>
+    </li>
+</ul>
+</body>
+</html>
